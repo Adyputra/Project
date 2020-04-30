37c65974554462e15bb34dfa46872f048d45e9c9
Nizarud <muhnizarudn@gmail.com>
2020-05-01 04:35:53 +0700

    profile percetakan

--- Bersama/application/views/profilepercetakan/index.php
+++ Bersama/application/views/profilepercetakan/index.php
@@ -21,6 +21,75 @@
 
 </div>
 <!-- /.container-fluid -->
+<div class="slider-item" style="background-image: url(images/bg-3.jpg);">
+      	<div class="overlay"></div>
+        <div class="container">
+          <div class="row slider-text justify-content-center align-items-center" data-scrollax-parent="true">
+            <div class="col-md-8 col-sm-12 text-center ftco-animate">
+            	<span class="subheading">Selamat Datang di</span>
+              	<h1 class="mb-4">Media Jaya</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <div class="container">
+    	<div class="one-half img" style="background-image: url(images/about-.jpg);"></div>
+    		<div class="one-half ftco-animate">
+    		<div class="overlap">
+	        <div class="heading-section ftco-animate ">
+	        	<span class="subheading">Media Jaya</span>
+	          	<h2 class="mb-4">Publishing - Printing</h2>
+	        </div>
+	        <div>
+	  			<p>Merupakan sebuah usaha percetakan yang melayani segala cetakan umum maupun instansi</p>
+				<p>Umum : Cetak Kalender, Undangan, Cetak Kotak, Penjilidan Biasa / Hard Cover, Sticker, dll.</p>
+				<p>Instansi : Penggandaan Naskah Soal dan Packing, Pembuatan Buku Tulis dan Administrasi Kelas, Sampul Raport, Foto Copy, dll.</p>
+	  		</div>
+  			</div>
+    	</div>
+    </div>	
+
+    <section class="ftco-section ftco-services">
+    	<div class="container">
+    		<div class="row">
+          	<div class="col-md-4 ftco-animate">
+            <div class="media d-block text-center block-6 services">
+              <div class="icon d-flex justify-content-center align-items-center mb-5">
+              	<span class="flaticon-choices"></span>
+              </div>
+              <div class="media-body">
+                <h3 class="heading">Mudah Untuk Order</h3>
+                <p>Tinggal pesan sesuai kebutuhan dan keinginan anda</p>
+              </div>
+            </div>      
+        </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="media d-block text-center block-6 services">
+              <div class="icon d-flex justify-content-center align-items-center mb-5">
+              	<span class="flaticon-delivery-truck"></span>
+              </div>
+              <div class="media-body">
+                <h3 class="heading">Pengiriman Tercepat</h3>
+                <p>Waktu pengerjaan dan pengiriman dilakukan dengan cepat</p>
+              </div>
+            </div>      
+        </div>
+          <div class="col-md-4 ftco-animate">
+            <div class="media d-block text-center block-6 services">
+              <div class="icon d-flex justify-content-center align-items-center mb-5">
+              	<span class="flaticon-choices"></span></div>
+              <div class="media-body">
+                <h3 class="heading">Kualitas</h3>
+                <p>Menggunakan kualitas yang terbaik</p>
+              </div>
+            </div>    
+          </div>
+        </div>
+    	</div>
+    </section>
+
+    
 
 </div>
 <!-- End of Main Content -->
