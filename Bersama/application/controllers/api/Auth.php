ca291b156fd685d123e0ac9f9b99b4acb937a333
adynugrahaputra <adynugrahaputra@gmail.com>
2020-05-07 03:32:24 +0700

    tambah api login dengan google dan ganti password

--- Bersama/application/controllers/api/Auth.php
+++ Bersama/application/controllers/api/Auth.php
@@ -27,6 +27,47 @@ class Auth extends REST_Controller
         $this->response($data, 200);
     }
 
+    public function logingoogle_post()
+    {
+        $email = $this->input->post('email', true);
+        $nama = $this->input->post('nama', true);
+        $idgoogle = $this->input->post('idgoogle', true);
+        $cek = $this->db->get_where('tbcustomer', ['email' => $email, 'daftar_via' => 1])->row_array();
+        if ($cek > 0) {
+
+            $data['respon'] = [
+                'status' => false,
+                'pesan' => "Email Ini Telah Terdaftar Via Non Akun Google. Silahkan Mengisi Form Login.",
+            ];
+        } else {
+            $cek = $this->db->get_where('tbcustomer', ['email' => $email, 'daftar_via' => 2])->row_array();
+            if ($cek > 0) {
+
+                $data['respon'] = [
+                    'status' => true,
+                    'pesan' => "Login Berhasil",
+                    'data' => $cek
+                ];
+            } else {
+                $arr = [
+                    'nama' => $nama,
+                    'email' => $email,
+                    'password' => $idgoogle,
+                    'daftar_via' => 2
+                ];
+                $this->GeneralModel->insertData("tbcustomer", $arr);
+                $cek = $this->db->get_where('tbcustomer', ['email' => $email, 'daftar_via' => 2])->row_array();
+                $data['respon'] = [
+                    'status' => true,
+                    'pesan' => "Login Berhasil",
+                    'data' => $cek
+                ];
+            }
+        }
+
+        $this->response($data, 200);
+    }
+
     public function reqotp_post()
     {
         $email = $this->input->post('email',true);
@@ -150,6 +191,7 @@ class Auth extends REST_Controller
                 'data' => [
                     'nama' => $cek['nama'],
                     'email' => $cek['email'],
+                    'daftar_via' => $cek['daftar_via'],
                 ],
                 'status' => true,
                 'pesan' => "Berhasil"
@@ -192,4 +234,38 @@ class Auth extends REST_Controller
 
         $this->response($data, 200);
     }
+
+
+    public function gantipassword_post($id)
+    {
+        $passlama = $this->input->post('passlama', true);
+        $passbaru = $this->input->post('passbaru', true);
+        $passkonfir = $this->input->post('passkonfir', true);
+        $cek = $this->db->get_where('tbcustomer', ['id' => $id, 'password' => $passlama])->row_array();
+        if ($cek > 0) {
+                if ($passbaru == $passkonfir) {
+
+                    $this->GeneralModel->updateData("tbcustomer", ['password' =>  $passbaru], $id, 'id');
+                    $data['respon'] = [
+                        'status' => true,
+                        'pesan' => "Password Akun Anda Berhasil Di Ganti"
+                    ];
+                } else {
+
+                    $data['respon'] = [
+                        'status' => false,
+                        'pesan' => "Konfimasi Password Salah"
+                    ];
+                }
+        } else {
+            $data['respon'] = [
+                'status' => false,
+                'pesan' => "Password Lama Salah"
+            ];
+        }
+
+        $this->response($data, 200);
+    }
+
+
 }
